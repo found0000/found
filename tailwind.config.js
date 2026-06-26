8754f71c8443dfbd5bcda143153bb0e7d3261bec
mohdabdullahmeraj <mohdabdullahmeraj1705@gmail.com>
2026-06-27 01:37:11 +0530

    bg colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     extend: {
       colors: {
         'primary-teal': '#008081',
-        'cream': 'rgba(255, 253, 247, 1)',
+        'cream': '#FDFDF5',
         'accent-coral': '#E3735F',
         'dark-teal': '#003435',
       },
